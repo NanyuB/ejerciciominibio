b7ed6da40c355b64080961b4dea3ffaeac084c02
Natalia Guerrero <nathaliiaa_best@hotmail.com>
2021-06-29 10:29:11 +0200

    'master'

---
+++ minibio/src/app/model/user.model.ts
@@ -0,0 +1,12 @@
+export class User {
+    image?:string;
+    name?: string;
+    descripcion?: string;
+    redes?: Redes;
+}
+export class Redes{
+    github?:string;
+    twitter?:string;
+    attom?:string;
+    linkedin?: string;
+}
